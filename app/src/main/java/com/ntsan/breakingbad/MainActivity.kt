c3f19a966036d3d2547442e156f8372425957bd3
NikTsanka <nik.tsanka@gmail.com>
2021-02-21 00:03:28 +0400

    added registration form

--- app/src/main/java/com/ntsan/breakingbad/MainActivity.kt
+++ app/src/main/java/com/ntsan/breakingbad/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.ntsan.breakingbad
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_activity)
+        window.setBackgroundDrawable(null)
+        supportActionBar?.hide()
+
+        val goToRegActivity = findViewById<TextView>(R.id.registrationTextView)
+        goToRegActivity.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/ntsan/breakingbad/RegistrationActivity.kt
@@ -0,0 +1,19 @@
+package com.ntsan.breakingbad
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registration_activity)
+
+        val goToLoginActivity = findViewById<TextView>(R.id.backButtonTextView )
+        goToLoginActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
