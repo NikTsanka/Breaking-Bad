58995dd56d3b9426a749ea07ecd30b0c04869d38
NikTsanka <nik.tsanka@gmail.com>
2021-03-05 18:07:54 +0400

    deleted regActivity and added fragments. added animation when moving to another fragments

--- app/src/main/java/com/ntsan/breakingbad/MainActivity.kt
+++ app/src/main/java/com/ntsan/breakingbad/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.ntsan.breakingbad
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_activity)
-        window.setBackgroundDrawable(null)
-        supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+       // window.setBackgroundDrawable(null)
+     //   supportActionBar?.hide()
 
-        val goToRegActivity = findViewById<TextView>(R.id.registrationTextView)
-        goToRegActivity.setOnClickListener {
-            val intent = Intent(this, RegistrationActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
---
+++ app/src/main/java/com/ntsan/breakingbad/fragments/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.ntsan.breakingbad.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.ntsan.breakingbad.R
+import kotlinx.android.synthetic.main.fragment_login.view.*
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        view.registrationTvLogScr.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
+        }
+
+
+
+        return view
+    }
+}
